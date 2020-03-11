c58926a9186fac44e99e1d5cfb3629201abe067a
Lautaro Carro <lautaroecarro@gmail.com>
2020-03-11 13:25:48 -0300

    add: Containers CRUD

---
+++ Server/Controllers/ContainersController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzureBlobStorageCrud.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContainersController : Controller
+    {
+        private readonly BlobServiceClient _client;
+
+        public ContainersController(BlobServiceClient client)
+        {
+            _client = client;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public async IAsyncEnumerable<string> GetAsync()
+        {
+            await foreach (BlobContainerItem container in _client.GetBlobContainersAsync())
+            {
+                yield return container.Name;
+            }
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task Post([FromBody]string name)
+        {
+            BlobContainerClient blobContainer = await _client.CreateBlobContainerAsync(name);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+            throw new InvalidOperationException();
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{name}")]
+        public async Task Delete(string name)
+        {
+            await _client.DeleteBlobContainerAsync(name);
+        }
+    }
+}
---
+++ Server/Startup.cs
@@ -0,0 +1,57 @@
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+
+namespace AzureBlobStorageCrud.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
+            services.AddSingleton(new BlobServiceClient(Configuration.GetConnectionString("AzureStorage")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseResponseCompression();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBlazorDebugging();
+            }
+
+            app.UseStaticFiles();
+            app.UseClientSideBlazorFiles<Client.Program>();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapFallbackToClientSideBlazor<Client.Program>("index.html");
+            });
+        }
+    }
+}
